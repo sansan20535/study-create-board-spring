274c6c2f333fce4fb9a8555cafe941ee98a6bcdc
sansan20535 <20san20@daum.net>
2024-02-18 13:17:37 +0900

    [Feat]Create boardwrite.html and Modify Controller

--- src/main/java/com/study/board/controller/BoardController.java
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -7,9 +7,15 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller //controller Annotation
 public class BoardController {
 
-    @GetMapping("/") //localhost:8090으로 접속했을 때 (기본)
+    /*@GetMapping("/") //localhost:8090으로 접속했을 때 (기본)
     @ResponseBody //return값을 그대로 띄워주는 Annotation
     public String main(){
         return "Hello World";
+    }*/
+
+    @GetMapping("/board/write") // /board/write로 접속했을 때
+    public String boardWriteForm(){
+
+        return "boardwrite"; //return된 이름을 가진 html파일 렌더링
     }
 }
