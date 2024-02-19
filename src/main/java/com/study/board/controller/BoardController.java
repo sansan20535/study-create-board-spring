96996800e05a395d3528062f395e851801a2ed8a
sansan20535 <20san20@daum.net>
2024-02-19 21:30:22 +0900

    [Feat]Create entity, repository package and Board Class

--- src/main/java/com/study/board/controller/BoardController.java
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -2,6 +2,8 @@ package com.study.board.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller //controller Annotation
@@ -13,9 +15,21 @@ public class BoardController {
         return "Hello World";
     }*/
 
-    @GetMapping("/board/write") // /board/write로 접속했을 때
-    public String boardWriteForm(){
+    @GetMapping("/board/write") // /board/write로 Get방식을 통해 접속했을 때
+    public String boardWriteForm() {
 
         return "boardwrite"; //return된 이름을 가진 html파일 렌더링
+    }
+
+    @PostMapping("/board/writepro") // /board/writepro로 Post방식을 통해 접속했을 때
+    public String boardWritePro(@RequestParam(name = "title") String title,@RequestParam(name = "content") String content) {
+        // @RequestParam을 통해 title과 content를 인자로 받음
+/*
+        log를 통해 확인
+        System.out.println("title = " + title);
+        System.out.println("content = " + content);
+*/
+
+        return "";
     }
 }
---
+++ src/main/java/com/study/board/entity/Board.java
@@ -0,0 +1,20 @@
+package com.study.board.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity //DB에 있는 Table을 의미
+@Data //lombok : id, title, content를 getId() 등의 메소드로 꺼낼 수 있게 함
+public class Board {
+
+    @Id //primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //IDENTITY : MariaDB, MySQL 
+    private Integer id; //DB내 게시글 번호
+
+    private String title; //DB내 게시글 제목
+
+    private String content; //DB내 게시글 내용
+}
